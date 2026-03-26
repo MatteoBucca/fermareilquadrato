34bee913c5d1b661bc1cd3c06a0f298abfa7e4f8
MatteoBucca <bucca.matteo@iisgalvanimi.edu.it>
2026-03-26 21:05:25 +0000

    finito

---
+++ fermare.js
@@ -0,0 +1,69 @@
+var redSquare;
+
+function startGame() {
+    redSquare = new component(20, 20, "red", 10, 120);
+    myGameArea.start();
+}
+
+var myGameArea = {
+    canvas: document.createElement("canvas"),
+    start: function() {
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20); 
+    },
+    clear: function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+};
+
+function component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    this.speedX = 0; 
+    this.speedY = 0;
+    
+    this.update = function() {
+        var ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    };
+
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+    };
+}
+
+function updateGameArea() {
+    myGameArea.clear();    
+    redSquare.newPos();    
+    redSquare.update();    
+}
+
+
+function moveup() {
+    redSquare.speedY = -1; 
+}
+
+function movedown() {
+    redSquare.speedY = 1;
+}
+
+function moveleft() {
+    redSquare.speedX = -1;
+}
+
+function moveright() {
+    redSquare.speedX = 1;
+}
+
+
+function clearmove() {
+    redSquare.speedX = 0;
+    redSquare.speedY = 0;
+}
